eddf21e21f4cfff7f06f2aba6b6432de3c1f09f6
Jake Engel <jakeengel01@gmail.com>
2017-10-16 16:37:52 +0200

    Reverted code to what it was before.

--- lib/app.rb
+++ lib/app.rb
@@ -1,18 +1,6 @@
 require_relative 'cookbook'    # You need to create this file!
 require_relative 'controller'  # You need to create this file!
 require_relative 'router'
-require "sinatra"
-require "sinatra/reloader" if development?
-require "pry-byebug"
-require "better_errors"
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
-end
-
-get '/' do
-  'Hello world!'
-end
 
 csv_file   = File.join(__dir__, 'recipes.csv')
 cookbook   = Cookbook.new(csv_file)
